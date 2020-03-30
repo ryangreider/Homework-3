b3b85e56eea1739c4ac896bd7b25172cc6f5d75d
ryangreider <ryandaniel136@hotmail.com>
2020-03-29 19:43:03 -0500

    Lists

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numberlist = [0,1,2,3,4,5,6,7,8,9];
+var lowercase = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+var uppercase = [A,B,C,D,E,F,G,H,U,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,8 +16,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 // Option ALerts
 generateBtn.addEventListener("click", function(){
-    alert("Here's some options for your password!");
-    prompt()
+    confirm("Do you want to generate a password?");
+    if (true) prompt("Length: How many characters will your password have?")
+    
 });
+
