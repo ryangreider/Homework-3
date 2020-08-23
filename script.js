913129681f05d7ec1a8732fe1832ab22539c2d59
ryangreider <ryandaniel136@hotmail.com>
2020-08-23 14:10:59 -0500

    Generate

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ generateBtn.addEventListener("click", writePassword);
 
 function promptForLength() {
   // Ask
-  const userLength = prompt('What length will your password be? (Minimum: 8');
+  const userLength = prompt('What length will your password be? (Minimum: 8)');
 
   // Verify
   if (!isNaN(userLength)) {
@@ -37,14 +37,46 @@ function confirmSpecialChar() {
   return confirm ('Special Characters?');
 }
 
+function confirmNumberChar() {
+  return confirm ('Numbers?');
+}
+
 function generatePassword() {
   const uppercaseChar = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
   const lowercaseChar = [...'abcdefghifjklmnopqrstuvwxyz'];
   const numberChar = [...'1234567890'];
   const specialChar = [ ..."!@#$%^&*()+-_.,~{}[]:'/\\"];
+
+  const passwordLength = promptForLength();
+  const wantsUppercase = confirmUppercase();
+  const wantsLowercase = confirmLowercase();
+  const wantsSpecialChar = confirmSpecialChar();
+  const wantsNumbers = confirmNumberChar();
+
+  // Result
+  let password = '';
+
+  while (true) {
+    if (wantsUppercase) {
+      password += uppercaseChar[Math.floor(Math.random() * uppercaseChar.length)];
+    } 
+
+    if (wantsLowercase) {
+      password += lowercaseChar[Math.floor(Math.random() * lowercaseChar.length)];
+    }
+
+    if (wantsSpecialChar) {
+      password += specialChar[Math.floor(Math.random() * specialChar.length)];
+    }
+
+    if (wantsNumbers) {
+      password += numberChar[Math.floor(Math.random() * numberChar.length)];
+    }
+
+    if (password.length >= passwordLength) {
+      break;
+    }
+
+  }
+  return password.slice(passwordLength);
 }
-// Prompts
-// 1- Length
-// 2- Uppercase
-// 3- lowercase
-// 4- special characters
